c753b339d68a66bd729d5ee3bd6b5b1258ae119d
Joshua Peek <josh@users.noreply.github.com>
2025-09-05 17:34:30 -0700

    Add basic age calling

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"os/exec"
 )
 
 // constants settable at build time.
@@ -29,6 +33,8 @@ type Output struct {
 }
 
 func main() {
+	ctx := context.Background()
+
 	if len(os.Args) > 1 && (os.Args[1] == "--version") {
 		fmt.Printf("tofu-age-encryption version %s\n", Version)
 		return
@@ -43,6 +49,13 @@ func main() {
 	if os.Args[1] != "--encrypt" && os.Args[1] != "--decrypt" {
 		fmt.Fprintf(os.Stderr, "usage: expected --encrypt or --decrypt\n")
 		os.Exit(1)
+	}
+
+	ageProgram := AgeProgram
+	if ageProgram == "" || ageProgram == "age" {
+		if path, err := exec.LookPath("age"); err == nil {
+			ageProgram = path
+		}
 	}
 
 	header := Header{
@@ -67,12 +80,22 @@ func main() {
 		log.Fatalf("Failed to parse stdin: %v", err)
 	}
 
+	// FIXME: Get age identity and recipient from env vars for now
+	ageIdentityFile := os.Getenv("AGE_IDENTITY_FILE")
+	ageRecipient := os.Getenv("AGE_RECIPIENT")
+
 	var outputPayload []byte
 	switch os.Args[1] {
 	case "--encrypt":
-		// TODO: encrypt the payload
+		outputPayload, err = ageEncryptPayload(ctx, ageProgram, ageRecipient, inputData.Payload)
+		if err != nil {
+			log.Fatalf("Failed to encrypt payload: %v", err)
+		}
 	case "--decrypt":
-		outputPayload = inputData.Payload
+		outputPayload, err = ageDecryptPayload(ctx, ageProgram, ageIdentityFile, inputData.Payload)
+		if err != nil {
+			log.Fatalf("Failed to decrypt payload: %v", err)
+		}
 	}
 
 	output := Output{
@@ -86,4 +109,46 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to write output: %v", err)
 	}
+}
+
+func ageEncryptPayload(ctx context.Context, ageProgram string, pubkey string, payload []byte) ([]byte, error) {
+	cmd := exec.CommandContext(ctx, ageProgram, "--encrypt", "--recipient", pubkey)
+	cmd.Stdin = bytes.NewReader(payload)
+
+	out, err := cmd.Output()
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			return nil, fmt.Errorf("timeout exceeded while encrypting payload with age")
+		}
+
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return nil, fmt.Errorf("%s", string(exitErr.Stderr))
+		}
+
+		return nil, fmt.Errorf("failed to encrypt payload with age: %w", err)
+	}
+
+	return out, nil
+}
+
+func ageDecryptPayload(ctx context.Context, ageProgram string, identityFile string, payload []byte) ([]byte, error) {
+	cmd := exec.CommandContext(ctx, ageProgram, "--decrypt", "--identity", identityFile)
+	cmd.Stdin = bytes.NewReader(payload)
+
+	out, err := cmd.Output()
+	if err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			return nil, fmt.Errorf("timeout exceeded while decrypting payload with age")
+		}
+
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return nil, fmt.Errorf("%s", string(exitErr.Stderr))
+		}
+
+		return nil, fmt.Errorf("failed to decrypt payload with age: %w", err)
+	}
+
+	return out, nil
 }
